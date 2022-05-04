31c51182ec23165ba0ff3d6d7c34795632e110ec
Procentguide2 <99178092+Procentguide2@users.noreply.github.com>
2022-05-04 19:50:12 +0300

    added form validation when creating/updating user

--- src/main/java/com/example/springcrud/model/User.java
+++ src/main/java/com/example/springcrud/model/User.java
@@ -3,6 +3,8 @@ package com.example.springcrud.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -13,12 +15,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotEmpty(message = "First name should not be empty")
+    @Size(min = 2, message = "First name should be more than 2 characters")
     @Column(name = "first_name")
     private String firstName;
 
+    @NotEmpty(message = "Last name should not be empty")
+    @Size(min = 2, message = "Last name should be more than 2 characters")
     @Column(name = "last_name")
     private String lastName;
 
+
     @Column(name = "phone")
+
     private long phone;
 }
